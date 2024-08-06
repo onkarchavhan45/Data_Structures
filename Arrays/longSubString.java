e227256ddc2e0ffcb3c4d67c9e85fa48efbe69b8
onkarchavhan45 <onkarchavhan45@gmail.com>
2024-08-06 17:34:55 +0530

    code with full exlaination

--- Arrays/longSubString.java
+++ Arrays/longSubString.java
@@ -26,3 +26,30 @@ public class longSubString {
         return max;
     }
 }
+
+/* Let's consider an example with the string s = "abcabcbb":
+
+Start with i = 0, j = 0, and max = 0.
+Iterate through the string:
+1)
+j = 0, character is 'a':
+'a' is not in the HashSet. Add 'a'.
+Update max = 1.
+2)
+j = 1, character is 'b':
+'b' is not in the HashSet. Add 'b'.
+Update max = 2.
+3)
+j = 2, character is 'c':
+'c' is not in the HashSet. Add 'c'.
+Update max = 3.
+4)
+j = 3, character is 'a':
+'a' is already in the HashSet. Remove 'a' (from i = 0), increment i to 1.
+Now, add 'a' (from j = 3).
+5)
+j = 4, character is 'b':
+'b' is already in the HashSet. Remove 'b' (from i = 1), increment i to 2.
+Now, add 'b' (from j = 4).
+6)
+Continue this process until the end of the string */
